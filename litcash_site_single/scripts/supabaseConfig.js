b45a38bb9275854829441ab22567429eac108fd9
njuraevforce-sketch <n.juraevforce@gmail.com>
2025-08-16 12:32:26 +0300

    Delete litcash_site_single directory

--- litcash_site_single/scripts/supabaseConfig.js
+++
@@ -1,5 +0,0 @@
-// Copy this to scripts/supabaseConfig.js and fill with your project values
-window.__SUPABASE__ = {
-  url: "https://einpfuegfsilnoiareco.supabase.co",
-  anon: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVpbnBmdWVnZnNpbG5vaWFyZWNvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQ0MjM3NzAsImV4cCI6MjA2OTk5OTc3MH0.6wF-8yaV38LrXH6Ptm3KIO34wRklJKbNCVoqIGNKh_w"
-};
