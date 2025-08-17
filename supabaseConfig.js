e9e4fbd7f13a978ec2bf5f5fb139bed7b69addc0
njuraevforce-sketch <n.juraevforce@gmail.com>
2025-08-17 23:21:48 +0300

    Delete supabaseConfig.js

--- supabaseConfig.js
+++
@@ -1,6 +0,0 @@
-// supabaseConfig.js
-window.SUPABASE_URL = "https://einpfuegfsilnoiareco.supabase.co";
-window.SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVpbnBmdWVnZnNpbG5vaWFyZWNvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQ0MjM3NzAsImV4cCI6MjA2OTk5OTc3MH0.6wF-8yaV38LrXH6Ptm3KIO34wRklJKbNCVoqIGNKh_w";
-
-// !! Никогда не используйте service_role в браузере !!
-// service_role и API KEY нужны только на серверной стороне (Edge Functions / админ-панель)
