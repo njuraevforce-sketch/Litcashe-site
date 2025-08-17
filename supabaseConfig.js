0a8d6d329e4447f68605cdec2f1ac9ae95c6c391
njuraevforce-sketch <n.juraevforce@gmail.com>
2025-08-17 13:42:36 +0300

    Update supabaseConfig.js

--- supabaseConfig.js
+++ supabaseConfig.js
@@ -5,3 +5,4 @@ window._supabase = supabase.createClient(
   "https://einpfuegfsilnoiareco.supabase.co",
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVpbnBmdWVnZnNpbG5vaWFyZWNvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQ0MjM3NzAsImV4cCI6MjA2OTk5OTc3MH0.6wF-8yaV38LrXH6Ptm3KIO34wRklJKbNCVoqIGNKh_w"
 );
+window.sb = window._supabase;
