eb8f9b6ba165800d510b496a03b056e5ee4e62eb
njuraevforce-sketch <n.juraevforce@gmail.com>
2025-09-11 05:12:39 +0300

    Update app_supabase.js

--- app_supabase.js
+++ app_supabase.js
@@ -105,7 +105,7 @@ const r1 = await sb.rpc('get_level_info');
 if (!r1.error && r1?.data) return r1?.data?.[0] ?? r1?.data;
     return null; 
    
- };
+
 
   LC.refreshLevelInfo = async function() {
     try {
